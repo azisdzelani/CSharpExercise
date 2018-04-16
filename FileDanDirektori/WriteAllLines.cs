bc6dc3f5fc9efa6e40e58606ab40c22c5165befd
azisdzaelani <azisdzelani@mhs.uinjkt.ac.id>
2018-04-16 23:49:48 +0700

    menambahkan code untuk menampilkan data dari file

--- FileDanDirektori/WriteAllLines.cs
+++ FileDanDirektori/WriteAllLines.cs
@@ -22,13 +22,21 @@ namespace FileDanDirektori
             string[] data = {"Baris pertama",
                              "Baris kedua",
                              "Baris ketiga",
-                             "Baris keempat"};
+                             "Baris keempat",
+                             "Baris Kelima"};
 
             //add data kedalam file
             try
             {
                 File.WriteAllLines(Path.Combine(path, namaFile), data);
                 Console.WriteLine("File {0} telah terbuat...", Path.Combine(path, namaFile));
+
+                //membaca data dari file
+                string[] datas = File.ReadAllLines(Path.Combine(path, namaFile));
+                foreach(string d in datas)
+                {
+                    Console.WriteLine(d + " ");
+                }
             }
             catch(IOException e)
             {
